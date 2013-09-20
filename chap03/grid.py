e12572cf0f4e0034b1816f35fa9e342053401369
Justin <justin.poh@students.olin.edu>
2013-09-20 12:04:07 -0400

    My solution for the grid exercise

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,19 @@
-"""Solution to an exercise in Think Python.
+""" 
+Solution to an exercise in Think Python.
 
-Author:
+Author: Justin Poh
 """
 
-print '+ - - - -'
+def grid(rows, columns):
+
+	r='+ - - - - '
+	c='|         '
+
+	fullR=r*columns+'+\n'
+	fullC=c*(columns+1)+'\n'
+
+	grid=(fullR+(fullC*4))*rows + fullR
+
+	print grid
+
+grid(5,5)
