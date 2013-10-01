aaafe04e981a357ab0b4a38f43f31f316d13f055
Justin <justin.poh@students.olin.edu>
2013-09-30 21:33:12 -0400

    Adjustin gthe code for curve.py

--- chap05/curve.py
+++ chap05/curve.py
@@ -8,41 +8,44 @@ turtle: the turtle object created using TurtleWorld from swampy
 n: the level of the dragon curve
 fd_len: the length that the turtle will travel each time
 
-Credit: This code was modified from http://www.comscigate.com/cs/IntroSedgewick/20elements/27recursion/Dragon.java.html
+Credit: This code was written using the L-system from http://en.wikipedia.org/wiki/L-system#Example_6:_Dragon_curve;
+This code was also written with the help of Julian(ninja).
 
 """
 
-def reverse_curve(turtle, n, fd_len):
+def dragon_start(turtle, n, fd_len):
+    angle=90
+    fd(turtle, fd_len)
+    dragon_x(turtle, n, fd_len)
+
+def dragon_x(turtle, n, fd_len):
+    angle=90
+    
+    if n==0:
+        return
+    
+    dragon_x(turtle, n-1, fd_len)
+    rt(turtle, angle)
+    dragon_y(turtle, n-1, fd_len)
+    fd(turtle, fd_len)
+
+def dragon_y(turtle, n, fd_len):
+
     angle=90
 
     if n==0:
-        fd(turtle, fd_len)
         return
 
-    else:
-        curve(turtle, n-1, fd_len)
-        lt(turtle, angle)
-        reverse_curve(turtle, n-1, fd_len)
+    fd(turtle, fd_len)
+    dragon_x(turtle, n-1, fd_len)
 
-def curve(turtle, n, fd_len):
-
-    angle=90
-
-    if n==0:
-
-        fd(turtle, fd_len)
-        return
-
-    else:
-        curve(turtle, n-1, fd_len) #This will ensure that the first dragon curve element will always be a forward version
-        rt(turtle, angle) #This will turn 90 degrees
-        reverse_curve(turtle, n-1, fd_len) #Ensures that if the programme is at the base unit, it draws a line, if it is not at the base unit, it alternates between turning left and right (between calling curve and reverse_curve)
+    lt(turtle, angle)
+    dragon_y(turtle, n-1, fd_len)
 
 world=TurtleWorld()
 bob=Turtle()
 bob.delay=0
 
-#koch(bob, 100)
-curve(bob, 3, 30)
+dragon_start(bob, 10, 30)
 
 wait_for_user()
