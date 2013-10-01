93e919a596531084265984304c00fd8bc5361666
Justin <justin.poh@students.olin.edu>
2013-10-01 11:59:38 -0400

    Adjusted recursion.py to capture the proper way to do stuff

--- LectureExercises/Lecture8/recursion.py
+++ LectureExercises/Lecture8/recursion.py
@@ -1,9 +1,22 @@
 def recurse(n, result=0):
-    if type(n)!= int:
-        return 'You did not provide an integer'
+    """ My not so good way to do it"""
+    
+    if type(n)!= int or n<0:
+        return 'You did not provide an integer or the number you provided is less than zero which is invalid'
     if n==0:
         return result
 
     return recurse(n-1, (n+result))
+
+def sum_series(n):
+
+    """ The technically proper way to implement this"""
+
+    if n==0:
+        return 0
+    
+    x=sum_series(n-1)
+
+    return x+n
 
 print recurse(4)
