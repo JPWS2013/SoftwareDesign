ce705d60b0f5a1752f5271a67c8b03d92224951c
Justin <justin.poh@students.olin.edu>
2013-10-07 22:21:31 -0400

    Modifying submission for chapter 7 homework

--- chap07/pi.py
+++ chap07/pi.py
@@ -10,21 +10,21 @@ def estimate_pi():
 	Returns the approximate value of pi
 	"""
 
-	result=1
-	summation=0
-	k=0.0
+	result = 1
+	summation = 0
+	k = 0.0
 
-	while abs(result)>(10**(-15)):
+	while abs(result) > (10 ** (-15)):
 
-		result1=(math.factorial(4.0*k)) * (1103.0+(26390.0*k))
-		result2=((math.factorial(k))**4.0) * (396.0**(4.0*k))
-		result=float(result1/result2)
+		result1 = (math.factorial(4.0 * k)) * (1103.0 + (26390.0 * k))
+		result2 = ((math.factorial(k)) ** 4.0) * (396.0 ** (4.0 * k))
+		result = float(result1 / result2)
 
-		summation=summation+result
+		summation=summation + result
 		k += 1
 
-	one_pi=((2.0*float(math.sqrt(2.0)))/9801.0)*summation
+	one_pi=((2.0 * float(math.sqrt(2.0))) / 9801.0) * summation
 
-	return 1/one_pi
+	return 1 / one_pi
 
 print estimate_pi()
