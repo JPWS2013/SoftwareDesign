b95357e9b9784f4caadcda1c2cceb22b682dd252
Justin <justin.poh@students.olin.edu>
2013-10-15 11:45:22 -0400

    Addinng stuff made during Lecture 12 class

---
+++ LectureExercises/Lecture12/remove_duplicates.py
@@ -0,0 +1,14 @@
+def remove_duplicates(origList):
+	newlist=[]
+
+	for eachElement in origList:
+		if eachElement not in newlist:
+			newlist.append(eachElement)
+
+	return newlist
+
+original=[1,2,3,4, 3, 6, 10,5,6,7, 3, 1, 2]
+
+unique=remove_duplicates(original)
+
+print unique
